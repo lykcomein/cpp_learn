a638dddfc4faf00a56401263d4e9f70c431040b0
lyk <1462709609@qq.com>
2024-01-03 17:59:17 +0800

    socket_test1.h_1

--- inc/socket_test1.h
+++ inc/socket_test1.h
@@ -4,7 +4,7 @@
 #include <iostream>
 
 extern int client_end_point();
-extern int server_and_point();
+extern int server_end_point();
 extern int create_tcp_socket();
 extern int create_acceptor_socket();
 extern int bind_acceptor_socket();
